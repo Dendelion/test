5f06ba307d33a6680e64479a0d43023f745ce4c1
Dendelion <sultanov-57@mail.ru>
2017-03-10 11:31:09 +0300

    Update bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -1,1 +1,13 @@
-asfdds
+#define CATCH_CONFIG_MAIN  
+#include <iostream>
+#include "catch.hpp"
+
+
+TEST_CASE( "Pow are computed", "[binpow]" ) {
+    REQUIRE( binpow(1,10) == 1 );
+    REQUIRE( binpow(2,5) == 32 );
+    REQUIRE( binpow(3,2) == 9 );
+    REQUIRE( binpow(10,8) == 100000000 );
+}
+
+
