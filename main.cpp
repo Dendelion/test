d1e3d583bf22e860370bbd8d8a2de6766442193e
Dendelion <sultanov-57@mail.ru>
2017-03-10 11:15:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 #include <stdlib>
+#include "binpow.h"
 using namespace std;
-
-int binpow (int a, int n) {
-	int res = 1;
-	while (n) {
-		if (n & 1)
-			res *= a;
-		a *= a;
-		n >>= 1;
-	}
-	return res;
-}
 
 int main()
 {
