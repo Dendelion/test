5434236a7bd01817ec4a45246c06b34f75f98920
Dendelion <sultanov-57@mail.ru>
2017-03-10 11:40:53 +0300

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,17 +1,7 @@
 #define CATCH_CONFIG_MAIN  
 #include <iostream>
 #include "catch.hpp"
-
-int binpow (int a, int n) {
-	int res = 1;
-	while (n) {
-		if (n & 1)
-			res *= a;
-		a *= a;
-		n >>= 1;
-	}
-	return res;
-}
+#include "mathTest.h"
 
 TEST_CASE( "Pow are computed", "[binpow]" ) {
     REQUIRE( binpow(1,10) == 1 );
