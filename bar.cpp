a53d9da6e887a23ac20860e763ebdb084e413546
Dendelion <sultanov-57@mail.ru>
2017-03-10 11:39:53 +0300

    Update bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -1,13 +1,13 @@
 #define CATCH_CONFIG_MAIN  
-#include <iostream>
 #include "catch.hpp"
+#include "mathTest.h"
 
 
-TEST_CASE( "Pow are computed", "[binpow]" ) {
-    REQUIRE( binpow(1,10) == 1 );
-    REQUIRE( binpow(2,5) == 32 );
-    REQUIRE( binpow(3,2) == 9 );
-    REQUIRE( binpow(10,8) == 100000000 );
+TEST_CASE( "factorial are computed", "[factor]" ) {
+    REQUIRE( binpow(1) == 1 );
+    REQUIRE( binpow(2) == 2 );
+    REQUIRE( binpow(3) == 6 );
+    REQUIRE( binpow(10) == 3628800 );
 }
 
 
